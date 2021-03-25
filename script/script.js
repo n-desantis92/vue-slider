e38061e45fb2f7afb4a85ef05b6b252c5fc571ee
n-desantis92 <dsnwebdev@gmail.com>
2021-03-25 17:00:16 +0100

    aggiunto evento click e aggiunto min-width css

--- script/script.js
+++ script/script.js
@@ -3,23 +3,39 @@ var app = new Vue({
 
     data: {
       img: [
-        {
-        url: "img/re-leone.jpg",
-        alt: "leone"
-      },
-        {
-        url: "img/mare-cocktail.jpg",
-        alt: "mare-cocktail"
-      },
-        {
-        url: "img/marte.jpeg",
-        alt: "marte"
-      },
-        {
-        url: "img/nemo.jpg",
-        alt: "nemo"
-      }
-    ]
+          {
+          url: "img/re-leone.jpg",
+          alt: "leone"
+        },
+          {
+          url: "img/mare-cocktail.jpg",
+          alt: "mare-cocktail"
+        },
+          {
+          url: "img/marte.jpeg",
+          alt: "marte"
+        },
+          {
+          url: "img/nemo.jpg",
+          alt: "nemo"
+        }
+      ],
+      imgIndex: 0,
     },
+    methods: {
+      nextImg: function () {
+        this.imgIndex++;
+        if (this.imgIndex == this.img.length) {
+          this.imgIndex = 0;
+        }
+      },
+      prevImg: function () {
+        this.imgIndex--;
+        if (this.imgIndex < 0 ) {
+          this.imgIndex = this.img.length - 1;
+        }
+      }
+    }
+
 
   });
